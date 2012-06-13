957b136385357acc6f1fb31114626f84c50a04ac
Élie Bouttier <cube.elie@gmail.com>
2012-06-13 02:46:41 +0200

    Release 1

--- sources/automate.c
+++ sources/automate.c
@@ -5,12 +5,10 @@ while (state < 9 && state > -1) {
         state = -1;
     } else {
         switch (state) {
-            // Attente du premier fanion
-            case 0:
+            case 0: // Attente du premier fanion
                 if (pending == FANION) {
                     state++; /* On passe a l'etat suivant */
                 } else {
-                    fprintf(stderr, "trame_decode: [...]\n");
                     state = -1;
                 }
                 break;
